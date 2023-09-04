2228b26e51ea50ff77664e731924ce5267afb639
ШухратовБе <marshiktop505@gmail.com>
2023-09-04 20:30:54 +0500

    do test_task

---
+++ 04.09.23/test.py
@@ -0,0 +1,20 @@
+# class Pizza():
+#     def __init__(self, prepare, cook, cut, wrap_up):
+#         self.prepare = prepare,
+#         self.cook = cook,
+#         self.cut = cut,
+#         self.wrap_up = wrap_up
+#         self.a = 'name'
+#         self.b = 'testo'
+#         self.c = 'sous'
+#         self.list_peperoni = ['kolbasa', 'big_testo', 'tomato']
+#         self.list_barbeku = ['shashlik', 'small_testo', 'cheese']
+#         self.list_dari_morya = ['mayonez', 'big_testo', 'fish']
+#
+#
+# d1 = Pizza('Подготовить все', 'испечь', 'порезать', 'упоковать', )
+
+# class Terminal(Pizza):
+#     def display_pizza(self):
+#         print('Пиццы: '.format(self.list_peperoni, self.list_barbeku, self.list_dari_morya))
+# print(Terminal.display_pizza())
