363ea7e686b6001a93a961987fd2343eaee87d66
ШухратовБе <marshiktop505@gmail.com>
2023-09-13 22:15:27 +0500

    add flask

---
+++ 13.09.2023/main.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+menu = [{}]
+
+
+@app.route('/', methods=['GET', 'POST'])
+def ind():
+    return render_template('index.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+
+@app.route('/skating')
+def skating():
+    return render_template('skating.html')
+
+
+if __name__ == '__main__':
+    app.run()
