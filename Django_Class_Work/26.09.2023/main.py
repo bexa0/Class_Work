143327de311d5dce1b03ae3dbf51a97d65398b29
bexa_XO <marshiktop505@gmail.com>
2023-09-26 21:22:09 +0500

    add 26.09.2023

---
+++ Django_Class_Work/26.09.2023/main.py
@@ -0,0 +1,37 @@
+import random
+# def task1():
+#     list1 = [1, 2, 3, 4, 5]
+#     b = ['-{}+'.format(number) for number in list1]
+#     print(b)
+# task1()
+
+# def task2():
+#     slovar = {'a' : 1, 'b' : 2, 'c' : 3}
+#     a = dict(reversed(slovar.items()))
+#     print(a)
+# task2()
+
+# def task3():
+#     text = 'Hi, my name is Ben'
+#     x = ' '.join(text)
+#     print(x)
+# task3()
+
+
+
+# def task6():
+#     b = random.randint(0, 15)
+#     print('Угадай число\nУ тебя есть три попытки')
+#     user = input()
+#     i = 0
+#     while i < 3:
+#         if b < user:
+#             print('Меньше')
+#             i += 1
+#         if b > user:
+#             print('Больше')
+#         if b == user:
+#             print('Ты угадал')
+#
+#
+# task6()
